f44f77a63f6fd6fe65e8c76e2584e30144c6ce52
shreya-kanodia <98578568+shreya-kanodia@users.noreply.github.com>
2022-08-30 08:14:55 +0530

    Some changes for assignment5

--- assignment5/server/code/db.py
+++ assignment5/server/code/db.py
@@ -1,47 +1,17 @@
-
-import csv
-
+import sqlite3
+from pathlib import Path
+import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine
 
 db=SQLAlchemy()
+Path("data.db").touch()
 
-engine = create_engine('sqlite:///data.db', echo=True)
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
 
-metadata = MetaData()
-# Define the table with sqlalchemy:
-salaries = Table('salaries', metadata,
-    Column('id',Integer, primary_key=True),
-    Column('work_year',String(200)),
-    Column('experience_level',String(50)),
-    Column('employment_type',String(20)),
-    Column('job_title',String(20)),
-    Column('salary',Integer),
-    Column('salary_currency',String(20)),
-    Column('salaryinusd',Integer),
-    Column('employee_residence',String(20)),
-    Column('remote_ratio',Integer),
-    Column('company_size',String(20)),
+salaries_data = pd.read_csv("./salaries.csv")
 
-)
-metadata.create_all(engine)
-insert_query = salaries.insert()
+salaries_data.to_sql("salaries", connection, if_exists="replace", index=False)
 
-
-with open('salaries.csv', 'r', encoding="utf-8") as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
-    engine.execute(
-        insert_query,
-        [{
-        "work_year": row[0],
-        "experience_level": row[1],
-        "employment_type": row[2],
-        "job_title": row[3],
-        "salary": row[4],
-        "salary_currency": row[5],
-        "salaryinusd": row[6],
-        "employee_residence": row[7],
-        "remote_ratio": row[8],
-        "company_size": row[9]}
-            for row in csv_reader]
-    )
+connection.commit()
+connection.close()
